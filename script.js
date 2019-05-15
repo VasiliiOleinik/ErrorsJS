e730f92d8f89137b2d90effafab7b2900b715c86
HotBun <goooneoooogle@gmail.com>
2019-05-15 15:31:17 +0300

    Errors

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// try = выполнится если все ок
+// catch = если в try будет ошибка выполнится это
+// finally = выполнится в любом случае
+
+let json = '{"id":2}';
+
+try {
+    let user = JSON.parse(json);
+    console.log(user);
+
+    if (!user.name) {
+        throw new Error("В этих данных нет имени");
+    }
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+
+    console.log(`Мы получили ошибку: ${error.name}`);
+} finally {
+    console.log('Я выполнюсь всегда');
+}
+console.log('А я буду работать дальше!');
